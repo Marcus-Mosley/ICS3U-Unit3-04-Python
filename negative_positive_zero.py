1f4a5f07bae08cc1ccb561a827813743c34e6593
Ubuntu <ubuntu@ip-172-31-91-123.ec2.internal>
2020-09-08 23:41:38 +0000

    Added in negative_positive_zero.py

---
+++ negative_positive_zero.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# Created by Marcus A. Mosley
+# Created on September 2020
+# This program checks if an integer is negative, positive, or zero
+
+
+import random
+
+
+def main():
+    # This function checks if an integer is negative, positive, or zero
+
+    # Input
+    integer = int(input("Enter an Integer: "))
+    print("")
+
+    # Process & Output
+    if integer > 0:
+        print("This integer is + (positive)!")
+    elif integer < 0:
+        print("This integer is - (negative)!")
+    else:
+        print("This integer is 0 (zero)!")
+
+
+if __name__ == "__main__":
+    main()
